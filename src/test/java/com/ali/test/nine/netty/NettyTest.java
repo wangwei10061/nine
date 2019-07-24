707d467211b881e7e59e7431e5e6cf6266a63110
wandou.ww <wandou.ww@alibaba-inc.com>
2019-07-24 10:03:12 +0800

    Netty

---
+++ src/test/java/com/ali/test/nine/netty/NettyTest.java
@@ -0,0 +1,39 @@
+package com.ali.test.nine.netty;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NettyTest {
+
+    @Before
+    public void setUp() {
+        new Thread(() -> {
+            try {
+                new NettyServer(9999).run();
+            } catch (Exception e) {
+                e.printStackTrace();
+                assertFalse(true);
+            }
+        }).start();
+    }
+
+    @Test
+    public void client() {
+        new Thread(() -> {
+            NettyClient client = new NettyClient();
+            try {
+                client.connect("127.0.0.1", 9999);
+            } catch (Exception e) {
+                e.printStackTrace();
+                assertFalse(true);
+            }
+        }).start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
